9b9d35d66671c30958e14c4c457e2c94c7142414
Renan Feliciano <renan.feliciano.rocha@gmail.com>
2022-03-15 15:12:22 -0300

    configura corpo
    AppBar configurado com texto "Transferencia" e um botão flutuante adicionado

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children:  <Widget>[
-          Text(
-            'Deliver features faster',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-          Column(
-            children: <Widget>[
-              Text(
-                'Deliver features faster',
-                textDirection: TextDirection.ltr,
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Transferências'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Icon(Icons.add),
+    ),
+  ),
+));
 
-// void main() {
-//   runApp(const MyApp());
-// }
