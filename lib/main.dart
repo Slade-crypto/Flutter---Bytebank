19ecd1b0900f9219a6ce7a28542ec0012d593043
Renan Feliciano <renan.feliciano.rocha@gmail.com>
2022-03-15 14:41:53 -0300

    meu primeiro commit do projeto bytebank

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      Column(
+        children:  <Widget>[
+          Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                'Deliver features faster',
+                textDirection: TextDirection.ltr,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+
+// void main() {
+//   runApp(const MyApp());
+// }
