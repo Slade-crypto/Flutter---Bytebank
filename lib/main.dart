279afad863712eaadbbbbaa8a6eb12787b9dff70
Renan Feliciano <renan.feliciano.rocha@gmail.com>
2022-03-16 15:04:56 -0300

    Lista de transações

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
+    body: Column(
+      children: [
+        Card(
+          child: ListTile(
+            title: Text('100.0'),
+            subtitle: Text('100.0'),
+            leading: Icon(Icons.monetization_on),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text('200.0'),
+            subtitle: Text('2000'),
+            leading: Icon(Icons.monetization_on),
+          ),
+        ),
+      ],
+    ),
     appBar: AppBar(
       title: Text('Transferências'),
     ),
